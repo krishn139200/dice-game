f8e1c176ec800275c4c39b4304be577be4bcec7a
krishn kant vashishtha <65147813+krishn139200@users.noreply.github.com>
2021-08-26 21:03:11 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var  randomnumber1 = Math.floor(Math.random()*6)+1; // take radom number 1 to 6
+
+var  randomimg= "dice" + randomnumber1 + ".png";
+
+var  randomimgsrc="images/" + randomimg;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomimgsrc);
+
+var  randomnumber2 = Math.floor(Math.random()*6)+1;
+
+randomimgsrc2="images/dice" + randomnumber2 +  ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomimgsrc2);
+
+if(randomnumber1>randomnumber2){
+    document.querySelector("h1").innerHTML = "player 1 is win"
+}
+else if(randomnumber2>randomnumber1) {
+    document.querySelector("h1").innerHTML = "player 2 is win"
+}
+else{
+    document.querySelector("h1").innerHTML = "match draw"
+}
